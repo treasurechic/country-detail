0671757aef4fb9eded903101c4c7c49e1731030a
Treasurechic <babychic140@gmail.com>
2022-07-31 18:40:38 +0100

    :hammer: add getcountrydetail function

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const countries = require("./countries");
 
 const getCountryDetail = (value) => {
-  return countries.find(
-    (country) => country.iso2 === value || country.iso3 === value || country.name === value
+  const cdnUrl =
+    "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/4.1.4/flags/4x3/";
+
+  const detail = countries.find(
+    (country) =>
+      country.iso2 === value || country.iso3 === value || country.name === value
   );
+  return {
+    ...detail,
+    flag: `${cdnUrl}${detail.iso2}.svg`
+  }
 };
 
 module.exports = getCountryDetail;
